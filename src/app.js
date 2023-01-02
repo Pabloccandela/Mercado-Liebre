3a3898d8c317e4d9b79f2a9e35874de34841f279
Pablo Candela <pabloccandela@gmail.com>
2023-01-02 19:16:10 -0300

    agregando pagina de mantenimiento

--- src/app.js
+++ src/app.js
@@ -22,6 +22,9 @@ app.get("/register",(req,res)=>{
 app.get("/login",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"../views/login.html"));
 })
+app.get("/mantenimiento",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../views/nodisponible.html"));
+})
 
 
 //Listen
